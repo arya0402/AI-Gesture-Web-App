32c0b0002b4a30104317e026886ca6965866774d
Aryadolphin <78508957+arya0402@users.noreply.github.com>
2022-09-18 17:18:43 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX = 0;
+noseY = 0;
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -13,10 +19,22 @@ function modelLoaded() {
 
 function draw() {
     background("#666464");
+    document.getElementById("square_size").innerHTML = "Size = " + difference + "px";
+    fill("#fc03ec");
+    stroke("#fc03ec");
+    square(noseX, noseY, difference);
+
 }
 
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + " noseY = " + noseY); 
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
     }
 }
